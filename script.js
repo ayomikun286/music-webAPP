ddc3df6c19bd5f057a8582178bded471124c68a7
AYOMIKUN <edegbaiayomikun@gmail.com>
2025-11-23 13:05:26 +0100

    update

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// const audio = document.getElementById('audio');
-// const playBTN = document.querySelector('#play');
-// const icon = document.querySelector('#play i');
-
-
-// playBTN.addEventListener('click', ()=>{
-//     if(audio.paused){
-//         audio.play();
-//         icon.classList.remove('fa-play');
-//         icon.classList.add('fa-pause');
-//     }else{
-//         audio.pause();
-
-//         icon.classList.remove('fa-pause')
-//         icon.classList.add('fa-play');
-
-//     }
-// });
 
 
 function playMUsic() {
@@ -41,6 +23,9 @@ function playMUsic() {
 
 }
 playMUsic();
+
+
+
 
 const cloudName = "doua1o7qw";
 const uploadPreset = "music_upload";
