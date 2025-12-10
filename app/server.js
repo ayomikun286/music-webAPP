06f13a2e83548e08eb04fca0d0e1306103363ad3
AYOMIKUN <edegbaiayomikun@gmail.com>
2025-12-10 19:00:07 +0100

    update for railway

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -56,7 +56,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 
-    app.listen(PORT, ()=>  console.log(`Server running on port ${PROT}`));
+    app.listen(PORT, ()=>  console.log(`Server running on port ${PORT}`));
 
 
     
